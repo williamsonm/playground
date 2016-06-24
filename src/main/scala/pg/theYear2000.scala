461a752c1b20ddca0d47723adf2d61d5a3f1c933
Matt Williamson <mattheww@pennwell.com>
2016-06-24 14:34:20 -0500

    Dunno what this is, side-effecting Futures or something.

---
+++ src/main/scala/pg/theYear2000.scala
@@ -0,0 +1,15 @@
+package pg
+
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object theYear2000 extends App {
+
+  val a = Future { Thread.sleep(1000); 2 }
+  val b = Future { Thread.sleep(1000); 4 }
+  val z = for (x <- a; y <- b) yield x * y
+  for (wut <- z) println("Result: " + wut)
+  println("really")
+  Thread.sleep(5000)
+  println("done")
+}
