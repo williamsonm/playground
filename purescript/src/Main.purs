197b8596fb37b98380737c892eb61d155d62de16
Matt Williamson <mattheww@pennwell.com>
2017-06-02 12:34:02 -0600

    some purescript stuff

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -1,8 +1,19 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, (+))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+
+apply :: forall a b. (a -> b) -> a -> b
+apply f x = f x
+
+infix 0 apply as $
+
+fz :: Int -> Int
+fz x = x + 1
+
+az :: Int
+az = fz $ 3
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
