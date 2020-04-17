cc7c0e701f8edb3e6c8e1ec108aa788d76a5be90
Matthew Williamson <matt.t.williamson@gmail.com>
2020-04-16 23:15:11 -0600

    json response

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,43 @@
-#![feature(proc_macro_hygiene)]
-#![feature(decl_macro)]
+#![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
 #[cfg(test)] mod tests;
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+use rocket_contrib::json::{Json, JsonValue};
+
+type ID = usize;
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    id: Option<ID>,
+    contents: String
+}
+
+#[get("/", format = "json")]
+fn hello() -> Json<Message> {
+    Json(Message {
+        id: Some(3),
+        contents: "whatever".to_string()
+    })
+}
+
+#[catch(404)]
+fn not_found() -> JsonValue {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![hello])
+        .register(catchers![not_found])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket().launch();
 }
