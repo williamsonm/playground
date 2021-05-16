e49acc9bb90e188176072525a3220ce0b707ae0d
Matthew Williamson <mwilliamson@twitter.com>
2021-05-15 20:30:32 -0500

    removing braces

--- scala/src/main/scala/pg/theYear2000.scala
+++ scala/src/main/scala/pg/theYear2000.scala
@@ -3,16 +3,17 @@ package pg
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
 
-object theYear2000 extends App {
+object theYear2000 extends App:
 
   val a = Future { Thread.sleep(1000); 2 }
   val b = Future { Thread.sleep(1000); 4 }
-  val z = for {
+  val z = for
     x <- a
     y <- b
-  } yield x * y
-  for (wut <- z) println("Result: " + wut)
+  yield x * y
+
+  for wut <- z do println(s"Result: $wut")
+
   println("really")
   Thread.sleep(5000)
   println("done")
-}
--- scala/src/test/scala/pg/BigDecimalSpec.scala
+++ scala/src/test/scala/pg/BigDecimalSpec.scala
@@ -4,13 +4,10 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-class BigDecimalSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks with should.Matchers {
+class BigDecimalSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks with should.Matchers:
 
-  "big decimal" should "what" in {
+  "big decimal" should "what" in
     forAll { (d: BigDecimal) =>
-      whenever (!(d.abs > 1 && d.toString.contains("E-"))) {
+      whenever (!(d.abs > 1 && d.toString.contains("E-")))
         println(s"$d")
-      }
     }
-  }
-}
